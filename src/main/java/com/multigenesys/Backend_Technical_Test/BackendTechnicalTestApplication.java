3fd91321dd37c05fcec0ceb469a90e1a17754789
Amol Bhosale <amolbhosale2022@gmail.com>
2024-08-08 12:51:58 +0530

    initial commit

---
+++ src/main/java/com/multigenesys/Backend_Technical_Test/BackendTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.multigenesys.Backend_Technical_Test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTechnicalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multigenesys/Backend_Technical_Test/BackendTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multigenesys.Backend_Technical_Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
